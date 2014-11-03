75a51bd518e6cdb5ee8cd699a67ef9c1cdcf30fa
Robert Heumueller <code@heum.de>
2014-11-03 11:54:24 +0100

    Added ast-evaluation to calculator

--- src/calculator.c
+++ src/calculator.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lexer.h"
+#include "hashmap.h"
+#include "ast_eval.h"
 #include "gram.h"
 #include "parser_state.h"
 
-void *ParseAlloc(void *(*allocProc)(size_t));
+void *ParseAlloc(void *(*allocProc) (size_t));
 void Parse(void *, int, const char *, struct parser_state *);
-void ParseFree(void *, void (*freeProc)(void *));
+void ParseFree(void *, void (*freeProc) (void *));
 
 int main()
 {
@@ -14,23 +16,35 @@ int main()
 	int r = 0;
 	int token;
 	struct parser_state parser_state;
+	struct hashmap *mappings = NULL;
 	yyscan_t scanner;
 	yylex_init(&scanner);
 
-	while(1) {
+	while (1) {
 		r = scanf("%s", s);
 		YY_BUFFER_STATE bufferState = yy_scan_string(s, scanner);
 
 		int token = yylex(scanner);
 		void *shellParser = ParseAlloc(malloc);
-		while(token > 0) {
-			Parse(shellParser, token, yyget_text(scanner), &parser_state);
-    	token = yylex(scanner);
-  	}
+		while (token > 0) {
+			Parse(shellParser, token, yyget_text(scanner),
+			      &parser_state);
+			token = yylex(scanner);
+		}
 
 		Parse(shellParser, 0, NULL, &parser_state);
+		ast_eval(parser_state.root, &mappings);
+		for (int i = 0; i < mappings->capacity; i++) {
+			if (mappings->values[i].key) {
+				printf("%s: %f\n", mappings->values[i].key,
+				       *((double *)mappings->values[i].value));
+			}
+		}
+		yy_delete_buffer(bufferState, scanner);
 		ParseFree(shellParser, free);
 	}
+
+	yylex_destroy(scanner);
 
 	return 0;
 }
